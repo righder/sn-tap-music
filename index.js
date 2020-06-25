3e59a0527e4e5d664567500bb70cbdfdc9b105da
righder <nayaksatyam98@gmail.com>
2020-06-25 23:45:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+
+    const colors = [
+        "#303130",
+        "#ee2929",
+        "#c620e7",
+        "#d3cf06",
+        "#02094b",
+        "#024e5c"
+    ];
+
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click", function () {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+            createBubble(index);
+        });
+    });
+
+    const createBubble = index => {
+
+        //Create bubbles
+
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = `jump 1s ease`;
+        bubble.addEventListener("animationend", function () {
+            visual.removeChild(this);
+        });
+    };
+});
